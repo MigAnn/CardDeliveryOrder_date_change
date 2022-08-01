1b75eeba0201bb1c6ebcc767d6a5210d71c6d17d
Anna Katkova <anakatkova166@gmail.com>
2022-08-01 21:11:34 +0300

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Card Delivery Order (data change)
+[![Build status](https://ci.appveyor.com/api/projects/status/0r0gjmrindd1uot8/branch/main?svg=true)](https://ci.appveyor.com/project/MigAnn/carddeliveryorder-date-change/branch/main)
--- src/test/java/test/DeliveryTest.java
+++ src/test/java/test/DeliveryTest.java
@@ -15,29 +15,29 @@ import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
 
-        @BeforeEach
-        void setup() {
-            open("http://localhost:9999");
-        }
-
-        @Test
-        @DisplayName("Should successful plan and replan meeting")
-        void shouldSuccessfulPlanAndReplanMeeting() {
-            var validUser = DataGenerator.Registration.generateUser("ru");
-            var daysToAddForFirstMeeting = 4;
-            var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-            var daysToAddForSecondMeeting = 7;
-            var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-            $("[data-test-id='city'] input").setValue(validUser.getCity());
-            $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-            $("[data-test-id='date'] input").setValue(secondMeetingDate);
-            $("[data-test-id='name'] input").setValue(validUser.getName());
-            $("[data-test-id='phone'] input").setValue(validUser.getPhone());
-            $("[data-test-id=agreement]").click();
-            $("[class='button__text']").click();
-            $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(15));
-            $("[data-test-id=success-notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + secondMeetingDate));
-
-
-        }
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
     }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(secondMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=success-notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + secondMeetingDate));
+
+
+    }
+}
